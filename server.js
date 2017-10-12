3914c4227f573e9c943daa15b35e5fd3d4a9f80d
kpack <kevinpackler@gmail.com>
2017-10-11 23:19:14 -0400

    change

--- server.js
+++ server.js
@@ -1,117 +1,51 @@
 
 var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
 var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 
-var Note = require("./models/note.js");
-var Article = require("./models/article.js");
 
-var request = require("request");
-var cheerio = require("cheerio");
+var PORT = process.env.PORT || 3000;
 
-mongoose.Promise = Promise;
 
 var app = express();
 
-app.use(logger("dev"));
+
+var router = express.Router();
+
+
+require("./config/routes")(router);
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/week18day3mongoose");
-var db = mongoose.connection;
+app.use(router);
 
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function(error) {
+
+  if (error) {
+    console.log(error);
+  }
+
+  else {
+    console.log("mongoose connection is successful");
+  }
 });
 
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
-app.get("/scrape", function(req, res) {
-  request("http://www.nytimes.com/", function(error, response, html) {
-    var $ = cheerio.load(html);
-    $("story-heading").each(function(i, element) {
-        var result = {};
-        result.title = $(this).children("a").text();
-        result.link = $(this).children("a").attr("href");
-        var entry = new Article(result);
-
-      entry.save(function(err, doc) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log(doc);
-        }
-      });
-
-    });
-console.log(entry)
-  });
-  res.send("Scrape Complete");
-});
-
-app.get("/articles", function(req, res) {
-
-  Article.find({}, function(error, doc) {
-
-    if (error) {
-      console.log(error);
-    }
-
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-app.get("/articles/:id", function(req, res) {
-
-  Article.findOne({ "_id": req.params.id })
-  .populate("note")
-
-  .exec(function(error, doc) {
-
-    if (error) {
-      console.log(error);
-    }
-
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-app.post("/articles/:id", function(req, res) {
-
-  var newNote = new Note(req.body);
-
-  newNote.save(function(error, doc) {
-
-    if (error) {
-      console.log(error);
-    }
-
-    else {
-       Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
- 
-      .exec(function(err, doc) {
- 
-        if (err) {
-          console.log(err);
-        }
-        else {
- 
-          res.send(doc);
-        }
-      });
-    }
-  });
-});
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(PORT, function() {
+  console.log("Listening on port:" + PORT);
 });
